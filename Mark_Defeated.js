b5287d361f949eb181c23351af49acd58523a603
Matthew Monahan <64717385+Kekilla0@users.noreply.github.com>
2020-08-11 18:07:50 -0400

    Add files via upload

--- Mark_Defeated.js
+++ Mark_Defeated.js
@@ -3,35 +3,36 @@
 //change to accept more than 1
 //change to work outside of combat
 
+(async ()=>{
+  let update = duplicate(game.combats.active)
+  for(let token of canvas.tokens.controlled)
+  {
+    if(token.data.overlayEffect === "icons/svg/skull.svg")
+    {
+      token.update({overlayEffect : null});
+      for(let combatant of update.combatants)
+      {
+        if(combatant.tokenId === token.id)
+        {
+          combatant.css = "";
+          combatant.defeated = false;
+          combatant.img = token.data.img;
+        }
+      }
+    }else{
+      token.update({overlayEffect : "icons/svg/skull.svg"});
+      for(let combatant of update.combatants)
+      {
+        if(combatant.tokenId === token.id)
+        {
+          combatant.css = "defeated";
+          combatant.defeated = true;
+          combatant.img = "icons/svg/skull.svg";
+        }
+      }
+    }
+  }
+  await game.combats.active.update(update);
+})();
 
-let update = duplicate(game.combats.active)
-if(token.data.overlayEffect === "icons/svg/skull.svg")
-{
-  token.update({overlayEffect : null});
-  for(let combatant of update.combatants)
-  {
-    if(combatant.token._id === token.id)
-    {
-      combatant.css = "";
-      combatant.defeated = false;
-      combatant.img = token.data.img;
-      console.log(combatant);
-    }
-  }
-  game.combats.active.update(update);
-}else{
-  token.update({overlayEffect : "icons/svg/skull.svg"});
-  for(let combatant of update.combatants)
-  {
-    if(combatant.token._id === token.id)
-    {
-      combatant.css = "defeated";
-      combatant.defeated = true;
-      combatant.img = "icons/svg/skull.svg";
-      console.log(combatant);
-    }
-  }
-  game.combats.active.update(update);
-}
-console.log(update);
-console.log(game.combats.active);
+
