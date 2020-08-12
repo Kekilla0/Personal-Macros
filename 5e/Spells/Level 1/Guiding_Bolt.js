1f1fb078b613f5744bd470718b25fb90046cc324
Kekilla0 <64717385+Kekilla0@users.noreply.github.com>
2020-08-11 22:27:55 -0400

    Adding Cleric Spells

--- 5e/Spells/Level 1/Guiding_Bolt.js
+++ 5e/Spells/Level 1/Guiding_Bolt.js
@@ -1,1 +1,25 @@
 //Guiding Bolt
+
+(()=>{
+  if(game.user.targets.size !== 1) return ui.notifications.error(`Please target exactly 1 token.`);
+  let macro = game.macros.getName("Toggle_Token_Icon");
+  let target = game.user.targets.values().next().value;
+
+  item.actor.useSpell(item).then((result)=>{
+    if(!result) return;
+    let content = result.data.content;
+    let level = parseInt(content.charAt(content.indexOf("data-spell-level")+18));
+
+    item.rollAttack().then(result => {
+      if(!result) return;
+
+      if(result.total >= target.actor.data.data.attributes.ac.value)
+      {
+        item.rollDamage({spellLevel : level}).then(result => {
+          if(!result) return;
+        });
+        macro.execute(target.id,item.data.img,0.1);
+      }
+    });
+  });
+})();
