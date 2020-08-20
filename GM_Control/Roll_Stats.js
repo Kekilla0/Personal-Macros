93c5de1ea6b4f1361689b9b5639d5c8d7d96d694
Kekilla0 <64717385+Kekilla0@users.noreply.github.com>
2020-08-20 08:21:40 -0400

    Update Roll_Stats.js

--- GM_Control/Roll_Stats.js
+++ GM_Control/Roll_Stats.js
@@ -1,37 +1,47 @@
 (()=>{
-  let statString = `4d6kh3`;
+  let statString = `4d6r1kh3`;
   let stats = Array(6).fill(0).map(e=>new Roll(statString).roll());
   
-  let stat_string_message = ``, total_average = 0, total_low = 0, total_high = 0;
+  let stat_string_message = ``, total_average = 0, total_low = 0, total_high = 0, total_header = 0, average_kept = 0, average_change = 0;
 
-  total_average = ((stats[0].parts[0].faces/2) + 0.5) * stats[0].parts[0].rolls.filter(i=> i?.discarded !== true).length;
+  average_kept = average(stats.map(x=> x.parts[0].rolls.filter(i=> i?.discarded !== true && i?.rerolled !== true).length));
+  average_change = average(stats.map(x=> x.parts[0].rolls.filter(i=>i?.discarded === true || i?.rerolled === true).length));
+  
+  total_average = (((stats[0].parts[0].faces/2) + 0.5) * average_kept) + average_change;
   total_low = Math.ceil(total_average - (stats[0].parts[0].faces/2));
   total_high = Math.ceil(total_average + (stats[0].parts[0].faces/2));
 
+  total_header = Math.max(...stats.map(x=> x.parts[0].rolls.length));
 
   let header = ``;
-  for(let i = 1; i <= stats[0].parts[0].rolls.length; i++)
+  for(let i = 1; i <= total_header; i++)
   {
     header += `<th>D${i}</th>`; 
   }
 
   for(let stat of stats)
   {
-    stat_string_message += `<tr>`; 
-    for(let roll of stat.parts[0].rolls)
+    stat_string_message += `<tr>`;
+    for(let i = 0; i < total_header; i++)
     {
-      stat_string_message += `<td style="${colorSetter(roll.roll,1,stat.parts[0].faces)}">${roll.roll}</td>`;      
-    }
+      let roll = stat.parts[0].rolls[i]?.roll;
+      if(roll)
+      {
+        stat_string_message += `<td style="${colorSetter(roll,1,stat.parts[0].faces)}">${roll}</td>`;
+      }else{
+        stat_string_message += `<td></td>`;
+      }         
+    } 
     stat_string_message += `<td style="border-left:1px solid #000; ${colorSetter(stat.total,total_low,total_high)}">${stat.total}</td></tr>`;
   }
 
   let content = `    
   <table style="text-align:center">
     <tr>
-      <th colspan="${stats[0].parts[0].rolls.length + 1}">New Ability Scores</th>
+      <th colspan="${total_header + 1}">New Ability Scores</th>
     </tr>
     <tr style="border-bottom:1px solid #000">
-      <th colspan="${stats[0].parts[0].rolls.length + 1}">${statString}</th>
+      <th colspan="${total_header + 1}">${statString}</th>
     </tr>
     <tr style="border-bottom:1px solid #000">
       ${header}
@@ -49,4 +59,9 @@ function colorSetter(number,low,high)
   if(number <= low) return `color:red`;
   if(number >= high) return `color:green`;
   return ``;
+}
+
+function average(nums)
+{
+  return nums.reduce((a,b) => (a+b))/nums.length;
 }
