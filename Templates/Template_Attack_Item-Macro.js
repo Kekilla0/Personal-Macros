1ecea0a4539c3ee8b9703d65c7e87aef9024f421
Matthew Monahan <64717385+Kekilla0@users.noreply.github.com>
2020-08-30 16:05:00 -0400

    Update Template_Attack_Item-Macro.js

--- Templates/Template_Attack_Item-Macro.js
+++ Templates/Template_Attack_Item-Macro.js
@@ -1,7 +1,6 @@
 (async () => {
+  if(game.user.target.size !== 1) return ui.notificiations.error(`Target 1 Token`);
   let target_actor = Array.from(game.user.targets)[0].actor;
-
-  if(!target_actor) return ui.notificiations.error(`Target 1 Token`);
 
   let attack_roll = await item.rollAttack();
   if(attack_roll.total >= target_actor.data.data.attributes.ac.value)
