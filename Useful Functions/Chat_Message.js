cbd17a042d46789a1b32d4cd38bcf229e9ca6ca2
Kekilla0 <64717385+Kekilla0@users.noreply.github.com>
2021-01-11 10:28:58 -0500

    Update Chat_Message.js

--- Useful Functions/Chat_Message.js
+++ Useful Functions/Chat_Message.js
@@ -11,28 +11,32 @@ async function sendMessage({ content, whisper = ChatMessage.getWhisperRecipients
 */
 async function doubleRoll({ rolls , title = ``, img = ``})
 {
-  const id = randomID();
+  let [a,b] = rolls;
+  if(!a || !b ) return;
+
   let template = `
-    <div style="display:flex; flex-direction:column;">
-      <div style="display:flex; flex-direction:row; flex-wrap:nowrap; justify-content:space-between;">
-        <img style="flex:1" src="${img}" title="${title}" width="36" height="36"/>
-        <h3 style="flex:3">${title}</h3>
-      </div>
+    <hr>
+    <div style="display:flex; flex-direction:row; flex-wrap:nowrap; justify-content:flex-start; align-items:center;">
+      <img style="flex : 0 0 36px; margin-right:5px" src="${img}" title="${title}" width="36" height="36"/>
+      <h3 style="flex : 1">${title}</h3>
+    </div>
+    <hr>
+    <div style="display:flex; flex-direction:row; flex-wrap:nowrap; justify-content:nospace; flex-grow:1">
+      <span style="flex:1; background:rgba(0, 0, 0, 0.1);border: 1px solid #999;border-radius: 4px;box-shadow: 0 0 2px #fff inset; font-size: 18px; text-align:center; padding: 8px 0px 8px 0px;">
+        ${a._formula}
+      </span>
+      <span style="flex:1; background:rgba(0, 0, 0, 0.1);border: 1px solid #999;border-radius: 4px;box-shadow: 0 0 2px #fff inset; font-size: 18px; text-align:center; padding: 8px 0px 8px 0px;">
+        ${b._formula}
+      </span>
+    </div>
+    <div style="display:flex; flex-direction:row; flex-wrap:nowrap; justify-content:nospace; flex-grow:1">
+      <span style="flex:1; background:rgba(0, 0, 0, 0.1);border: 1px solid #999;border-radius: 4px;box-shadow: 0 0 2px #fff inset; font-size: 18px; text-align:center; padding: 8px 0px 8px 0px;">
+        ${a.total}
+      </span>
+      <span style="flex:1; background:rgba(0, 0, 0, 0.1);border: 1px solid #999;border-radius: 4px;box-shadow: 0 0 2px #fff inset; font-size: 18px; text-align:center; padding: 8px 0px 8px 0px;">
+        ${b.total}
+      </span>
     </div>`;
-    /*
-      <div>
-        <div>
-          Roll 1 {Formula}
-        </div>
-        <div>
-          Roll 2 {Formula}
-        </div>
-      </div>
-      <div>
-        Total
-      </div>
-    </div>`;
-    */
 
   return await ChatMessage.create({ content : template });
 }
