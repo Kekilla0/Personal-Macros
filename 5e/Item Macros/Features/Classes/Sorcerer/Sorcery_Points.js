1712cd20aa73cbe3834b1256ff592dcd43adf155
Kekilla0 <64717385+Kekilla0@users.noreply.github.com>
2021-01-13 18:53:37 -0500

    Major Changes to Sorcery Points Macro

--- 5e/Item Macros/Features/Classes/Sorcerer/Sorcery_Points.js
+++ 5e/Item Macros/Features/Classes/Sorcerer/Sorcery_Points.js
@@ -1,279 +1,243 @@
-/*sorcerer point macro
- *Module Requirements : Furnace
- *Character Requirements : 	Must be a user with levels in "Sorcerer".
- *				Must have a feature with uses equal to sorcerer level named "Sorcery Points"
- *				Metamagic Feats must be named as they are in the Macro.
- */
-let outlog =(...args) => console.log("Sorcerer | ", ...args);
-let debug = false;
-//error checking
-if(game.user.character === null) return ui.notifications.warn(`Go away GM you are drunk.`);
-if(game.user.character.data.items.find(i=>i.name==="Sorcerer")===undefined) return ui.notifications.warn(`Your character does not have a Sorcerer Class.`);
-//variable creation
-let s_actor = game.user.character;
-let s_class = s_actor.data.items.find(i=>i.name==="Sorcerer");
-let s_points = s_actor.data.items.find(i=>i.name==="Sorcery Points");
-let s_slots = s_actor.data.data.spells;
-let confirmed = false;
-let fail = true;
+/*
+  Sorcery Point Macro
+  Requirements : 
+    1. Character or Token must possess 
+      A. A level in "Sorcerer"
+      B. Item named "Sorcery Points"
+      C. Optional : Items appropriately named for Specific Metamagics
+*/
 
-//dialog
-Sorcerer_Dialog();
+let log = (...args) => console.log("Sorcery Point Macro | ", ...args);
+let wait = async (ms) => new Promise((resolve)=> setTimeout(resolve, ms));
+let macroActor = character !== null ? character : token.actor;
+let data = { items : [], spellSlots : [], sorcPoints : {}, spells : [], cost : [2,3,5,6,7]};
+let display_message = true;
+let dialog_id = randomID();
 
-//functions
-function Sorcerer_Dialog()
-{
-	if(s_class.data.levels > 1 && s_class.data.levels !== s_points.data.uses.max) return ui.notifications.warn(`Sorcery Points value is incorrect.`);
-	let content = `
-		<div class = "form-group">
-			<br>
-			<label>Select Action  : </label>
-			<select id="use" name="use">`;
-	if(checkSlots("available") && s_points.data.uses.value !== s_points.data.uses.max) {content += `<option value = "sorcPoint">Spell Slot => Sorcery Point</option>`;}
-	if(checkSlots("missing") && s_points.data.uses.value >= 2){content += `<option value = "spellSlot">Sorcery Point => Spell Slot</option>`;}
+(async ()=>{
+  await dataCheck();
 
-	if(checkPoints() > 0 && checkSlots("available"))
-	{
-		content += `<option value = "extend">Extended Spell</option>`;
-		content += `<option value = "twin">Twinned Spell</option>`;
-	}
-	content += `</select><br><br></div>`;
+  log(data);
 
-	new Dialog({
-		title : "Sorcery Points Spender",
-		content : content,
-		buttons : {
-			one : {
-				icon :`<i class="fas fa-check"></i>`,
-				lable : "Continue",
-				callback : () => confirmed = true
-			},
-			two : {
-				icon : `<i class="fas fa-times"></i>`,
-				lable : "Cancel",
-				callback : () => confirmed = false
-			}
-		},
-		default : "Cancel",
-		close : html => {
-			if(confirmed){
-				switch(html.find('[name=use]')[0].value){
-					case "sorcPoint" : 
-						game.dnd5e.rollItemMacro("Spell to Sorcery Points").then(()=>{
-							spell_SorceryPoints();
-						});
-						break;
-					case "spellSlot" :
-						game.dnd5e.rollItemMacro("Sorcery Points to Spell").then(()=>{
-							sorceryPoints_spell();
-						});
-						break;
-					case "extend" :
-						game.dnd5e.rollItemMacro("Metamagic: Extended Spell").then(()=>{
-							metaMagic_Extended();
-						});
-						break;
-					case "twin" :
-						game.dnd5e.rollItemMacro("Metamagic: Twinned Spell").then(()=>{
-							metaMagic_Twinned();
-						});						
-						break;
-				}
-			}
-		}
-	}).render(true);
-}
-function spell_SorceryPoints()
-{
-	//variable creation
-	confirmed = false;
-	let newContent = `<div class = "form-group">
-						<br>
-						<label>Select Level  : </label>
-						<select id="lvl" name="lvl">`;
-	for(let slot in s_slots)
-	{
-		if(s_slots[slot].value !== 0 && s_slots[slot].value !== undefined)
-		{
-			newContent += `<option value = "${slot}">Spell Slot Level ${slot.charAt(5)} - ${s_slots[slot].value} </option>`;
-		}
-	}
-	newContent += `</select></div>`;
-	//Dialog and Logic
-	new Dialog({
-		title : "Creating Sorcery Points",
-		content : newContent,
-		buttons : {
-			one : {
-				icon :`<i class="fas fa-check"></i>`,
-				lable : "Continue",
-				callback : () => confirmed = true
-			},
-			two : {
-				icon : `<i class="fas fa-times"></i>`,
-				lable : "Cancel",
-				callback : () => confirmed = false
-			}
-		},
-		default : "Cancel",
-		close : html => {
-			if(confirmed)
-			{
-				//variable creation
-				let lvlchosen = html.find('[name=lvl]')[0].value;
-				let actorUpdateData = duplicate(s_actor);
-				let itemUpdateData = duplicate(s_points);
-				//This is where you would want to check for validity of use, maybe even a dialog to stop if unnecessary.
-				actorUpdateData.data.spells[lvlchosen].value -= 1;
-				itemUpdateData.data.uses.value += Math.clamped(parseInt(lvlchosen.charAt(5)),0,itemUpdateData.data.uses.max);
-				//character and item update
-				s_actor.update(actorUpdateData);
-				s_actor.updateEmbeddedEntity("OwnedItem", itemUpdateData);
-				//Display Information
-				display(`Succesfully Used a : Level ${lvlchosen.charAt(5)} Spell Slot <br> Creating : ${lvlchosen.charAt(5)} sorcery points.`);
-			}
-		}
-	}).render(true);
-}
-function sorceryPoints_spell()
-{
-	//variable creation
-	confirmed = false;
-	let s_cost = [2,3,5,6,7];
-	let newContent = `<div class = "form-group">
-						<br>
-						<label>Select Level  : </label>
-						<select id="lvl" name="lvl">`;
-	for(let slot in s_slots)
-	{
-		if(parseInt(slot.charAt(5)) <= Math.ceil(s_class.data.levels/2) && parseInt(slot.charAt(5)) < 6)
-		{
-			if(s_points.data.uses.value >= s_cost[parseInt(slot.charAt(5)-1)] && s_slots[slot].value !== s_slots[slot].max)
-			{ 
-				newContent += `<option value="${slot}">Spell Slot Level ${slot.charAt(5)} - ${s_slots[slot].value}</option>`;
-			}
-		}
-	}
-	newContent += `</select></div>`;
-	new Dialog({
-		title : "Creating Spell Slots",
-		content : newContent,
-		buttons : {
-			one : {
-				icon :`<i class="fas fa-check"></i>`,
-				lable : "Continue",
-				callback : () => confirmed = true
-			},
-			two : {
-				icon : `<i class="fas fa-times"></i>`,
-				lable : "Cancel",
-				callback : () => confirmed = false
-			}
-		},
-		default : "Cancel",
-		close : html => {
-			if (confirmed){
-				let lvlchosen = html.find('[name=lvl]')[0].value;
-				let actorUpdateData = duplicate(s_actor);
-				let itemUpdateData = duplicate(s_points);
-				actorUpdateData.data.spells[lvlchosen].value += 1;
-				itemUpdateData.data.uses.value -= Math.clamped((s_cost[parseInt(lvlchosen.charAt(5)-1)]),0,itemUpdateData.data.uses.max);
-				s_actor.update(actorUpdateData);
-				s_actor.updateEmbeddedEntity("OwnedItem", itemUpdateData);
-				display(`Succesfully Used : ${s_cost[parseInt(lvlchosen.charAt(5)-1)]} sorcery points. <br> Creating : Spell Slot Level ${lvlchosen.charAt(5)}`);
-			}			
-		}
-	}).render(true);
-}
-function metaMagic_Extended()
-{
-	if(game.user.character.data.items.find(i=>i.name==="Metamagic: Extended Spell")===undefined) return ui.notifications.warn(`Your character does not have a Metamagic: Extended Spell.`);
-	game.dnd5e.rollItemMacro("Metamagic: Extended Spell").then(()=> {
-		let itemUpdateData = duplicate(s_points);
-		itemUpdateData.data.uses.value -= 1;
-		s_actor.updateEmbeddedEntity("OwnedItem", itemUpdateData);
-	});
+  updateDialog();
+})();
 
-}
-function metaMagic_Twinned()
+  //add more information
+
+  //execute choice
+
+  //display choice (if the user wants it)
+
+
+async function dataCheck()
 {
-	if(game.user.character.data.items.find(i=>i.name==="Metamagic: Twinned Spell")===undefined) return ui.notifications.warn(`Your character does not have a Metamagic: Twinned Spell.`);
-	confirmed = false;
-	let newContent = `<div class = "form-group">
-	<br>
-	<label>Select Level  : </label>
-	<select id="lvl" name="lvl">`;
-	for(let slot in s_slots)
-	{
-		if(parseInt(slot.charAt(5)) <= s_points.data.uses.value)
-		{
-			newContent += `<option value="${slot}">Spell Slot Level ${slot.charAt(5)} - ${s_slots[slot].value}</option>`;
-		}
-	}
-	newContent += `</select></div>`;
-	new Dialog({
-		title : "Twinned Spell",
-		content : content,
-		buttons : {
-			one : {
-				icon :`<i class="fas fa-check"></i>`,
-				lable : "Continue",
-				callback : () => confirmed = true
-			},
-			two : {
-				icon : `<i class="fas fa-times"></i>`,
-				lable : "Cancel",
-				callback : () => confirmed = false
-			}
-		},
-		default : "Cancel",
-		close : html => {
-			if(confirmed)
-			{
-				game.dnd5e.rollItemMacro("Metamagic: Twinned Spell").then(()=>{
-					let lvlchosen = html.find('[name=lvl]')[0].value;
-					let itemUpdateData = duplicate(s_points);
-					itemUpdateData.data.uses.value -= parseInt(lvlchosen.charAt(5));
-					s_actor.updateEmbeddedEntity("OwnedItem", itemUpdateData);
-				});
-			}
-		}
-	}).render(true);
+  let items = [
+    "Sorcerer", 
+    "Font of Magic", 
+    "Metamagic: Careful Spell", 
+    "Metamagic: Distant Spell", 
+    "Metamagic: Empowered Spell", 
+    "Metamagic: Extended Spell", 
+    "Metamagic: Heightened Spell", 
+    "Metamagic: Quickened Spell", 
+    "Metamagic: Subtle Spell", 
+    "Metamagic: Twinned Spell"
+  ];
+
+  if(!macroActor) return error(`No Actor Selected (Character or Token)`);
+
+  items.forEach(name =>{
+    let item = macroActor.items.getName(name);
+
+    if(!item) data.items.push({_id : null, name });
+    else data.items.push({_id : item.id, name});
+  });
+
+  Object.entries(macroActor.data.data.spells).forEach(([key, {value, max}])=>{
+    if(key !== `pact` && max !== 0) data.spellSlots.push({ slot : parseInt(key.charAt(5)), value, max });
+    if(key === `pact` && max !== 0) data.spellSlots.push({ slot : `pact`, value, max });
+  });
+
+  if(!data.items.find(i=> i.name === `Sorcerer`)) return error(`${macroActor.name} does not have a level in Sorcerer`);
+  if(!data.items.find(i=> i.name === `Font of Magic`)) return error(`${macroActor.name} does not have the Font of Magic item/feature.`);
+
+  await fixFont();
+
+  // add checkSlotsAvailable, checkSlotsMissing here --- maybe?
+
+  // populate a new variable in data --- one for spells
+
+  async function fixFont()
+  {
+    let font = macroActor.items.getName("Font of Magic");
+    let { levels } = macroActor.items.getName("Sorcerer").data.data;
+    let { uses} = font.data.data;
+
+    if(uses.max !== levels)
+    {
+      let difference = levels - uses.max;
+      uses.max = levels;
+      uses.value = levels - difference;
+      await font.update({ "data.uses" : uses });
+    }
+    data.sorcPoints = uses;
+  }
 }
-function display(data =""){
-	if(data!=="")
-	{
-		ChatMessage.create({
-			user : game.user._id,
-			content : data,
-			speaker : speaker//, //GM whisper (to not make the chat window huge)
-			//whisper : game.users.entities.filter(u=>u.isGM).map(u=>u._id)
-		});
-	}
-}
-function checkSlots(data = "")
+function checkSlotsAvailable()
 {
-	if(data === "available")
-	{
-		for(let slot in s_slots)
-		{
-			if(s_slots[slot].value > 0)
-			{
-				return true;
-			}
-		}
-	}else if (data === "missing")
-	{
-		for(let slot in s_slots)
-		{
-			if(s_slots[slot].value !== s_slots[slot].max)
-			{
-				return true;
-			}
-		}
-	}
-	return false;
+  return data.spellSlots.reduce((acc, val) => val.value > 0 || acc, false);
 }
-function checkPoints(){
-	return s_points.data.uses.value;
+function checkSlotsMissing()
+{
+  return data.spellSlots.reduce((acc, val) => val.value !== val.max || acc, false);
 }
+function error(message)
+{
+  ui.notifications.error(message);
+  return new Error(message);
+}
+async function updateDialog()
+{
+  let interval, content, buttons, dialog, interval_time, last_action;
+  
+  interval_time = 0.2;
+
+  content = getContent();
+  buttons = { Ok : { label : `Ok`, callback : (html) => { executeHTML(html); }}, Cancel : { label : `Cancel` } };
+
+  dialog = new Dialog({ title : `Sorcerer Point Macro`, content, buttons, close : ()=> { clearInterval(interval); }}).render(true);
+
+  interval = setInterval(()=>{
+    let {action} = getHTML();
+
+    if(last_action !== action)
+    {
+      last_action = action;
+      update();
+    }
+  }, interval_time * 1000);
+
+  function getContent()
+  {
+    return `
+    <div style="display:flex; flex-direction:column;">
+      <div style="display:flex; flex-direction:row; justify-content:space-between; align-items:center; flex-grow:2;">
+        <img style="flex: 0 0 36px; margin-right : 5px; justify-content:flex-start;" src="${macroActor.data.img}" width="42" height="42"/>
+        <h3 style="flex : 1; justify-content:flex-end">${macroActor.data.name} </h3>
+      </div>
+      <div style="display:flex; flex-direction:row; justify-content:space-between; align-items:center; flex-grow:1;">
+        <h3 style="flex : 3; justify-content:flex-start;">spellSlots : ${data.spellSlots.map(ss=> ss.value).join(`/`)}</h3>
+        <h3 style="flex : 2; justify-content:flex-end;">Sorcery Points : ${data.sorcPoints.value}/${data.sorcPoints.max}</h3>
+      </div>
+      ${getActions()}
+      ${getSecondaryActions()}
+    </div>`;
+  }
+
+  function update()
+  {
+    dialog.data.content = getContent();
+    dialog.render(true);
+  }
+  
+  function getHTML()
+  {
+    let action = $.find(`#${dialog_id}actions`)[0]?.value;
+    let second = $.find(`#${dialog_id}secondary`)[0]?.value;
+
+    return { action, second };
+  }
+
+  function getActions()
+  {
+    let { action, second } = getHTML();
+
+    return `
+      <div style="display:flex; flex-direction:row; justify-content:space-between; align-items:center; flex-grow:1;">
+        <h3 style="flex : 1; justify-content:flex-start;"><label>Select Action : </label></h3>
+        <h3 style="flex : 1; justify-content:flex-end;">
+          <select id="${dialog_id}actions">
+            ${(data.sorcPoints.value !== data.sorcPoints.max && checkSlotsAvailable()) ? `<option ${action === `sp` ? `selected` : ``} value="sp">Spell Slot => Sorcery Point</option>` : ``}
+            ${(data.sorcPoints.value > 2 && checkSlotsMissing()) ? `<option ${action === `ss` ? `selected` : ``} value="ss">Sorcery Point => Spell Slot</option>` : ``}
+            ${(data.sorcPoints.value > 0 && checkAction(`Metamagic: Careful Spell`)) ? `<option ${action === `cr` ? `selected` : ``} value="cr">Careful Spell</option>` : ``}
+            ${(data.sorcPoints.value > 0 && checkAction(`Metamagic: Distant Spell`)) ? `<option ${action === `ds` ? `selected` : ``} value="ds">Distant Spell</option>` : ``}
+            ${(data.sorcPoints.value > 0 && checkAction(`Metamagic: Empowered Spell`)) ? `<option ${action === `em` ? `selected` : ``} value="em">Empowered Spell</option>` : ``}
+            ${(data.sorcPoints.value > 0 && checkAction(`Metamagic: Extended Spell`)) ? `<option ${action === `ex` ? `selected` : ``} value="ex">Extended Spell</option>` : ``}
+            ${(data.sorcPoints.value > 3 && checkAction(`Metamagic: Heightened Spell`)) ? `<option ${action === `hi` ? `selected` : ``} value="hi">Heightened Spell</option>`: ``}
+            ${(data.sorcPoints.value > 1 && checkAction(`Metamagic: Quickened Spell`)) ? `<option ${action === `qu` ? `selected` : ``} value="qu">Quickened Spell</option>` : ``}
+            ${(data.sorcPoints.value > 1 && checkAction(`Metamagic: Subtle Spell`)) ? `<option ${action === `sb` ? `selected` : ``} value="sb">Subtle Spell</option>` : ``}
+            ${(data.sorcPoints.value > 0 && checkAction(`Metamagic: Twinned Spell`)) ? `<option ${action === `tw` ? `selected` : ``} value="tw">Twinned Spell</option>` : ``}
+          </select>
+        </h3>
+      </div>
+      `;
+  }
+  function getSecondaryActions()
+  {
+    let { action, second } = getHTML(), label = ``, options = ``;
+
+    // build secondary list for dialog
+    switch(action)
+    {
+      case `sp` :
+      case `tw` :
+        label = `Pick Spell Slot : `;
+        options = data.spellSlots
+          .filter(({value})=> value!==0)
+          .map(({slot})=>`<option ${second === slot.toString() ? `selected` : ``} value="${slot}">Level ${slot}</option>`).join(``);
+
+        if(action === `tw`)
+          options = `<option ${second === "0" ? `selected` : ``} value="0">Cantrip</option>${options}`;
+        break;
+      case `ss` :
+        label = `Pick Spell Slot : `;
+        options = data.spellSlots
+          .filter(({slot, value, max})=> slot < 6 && data.cost[slot-1] <= data.sorcPoints.value && value < max)
+          .map(({slot})=> `<option ${second === slot.toString() ? `selected` : ``} value="${slot}">Level ${slot}</option>`).join(``);
+        break;
+      case `cr` :
+      case `ds` : 
+      case `em` : 
+      case `ex` :
+      case `hi` :
+      case `qu` :
+      case `sb` :
+        label = options = ``;
+        break;
+      default : 
+        label = options = `Error`;
+        break;
+    }
+
+    return label === `` ? `` : `
+      <div style="display:flex; flex-direction:row; justify-content:space-between; align-items:center; flex-grow:1;">
+        <h3 style="flex : 1; justify-content:flex-start;"><label>${label}</label></h3>
+        <h3 style="flex : 1; justify-content:flex-end;">
+          <select id="${dialog_id}secondary">
+            ${options}
+          </select>
+        </h3>
+      </div>
+      `;
+  }
+  function checkAction(name)
+  {
+    return data.items.find(i=>i.name === name)._id !== null;
+  }
+  async function executeHTML(html)
+  {
+    let html_data = {
+      action : html.find(`#${dialog_id}actions`)[0]?.value,
+      second : html.find(`#${dialog_id}secondary`)[0]?.value
+    };
+
+
+    //execute and update based on what the user chose
+
+    log("Action     | ", html_data.action);
+    log("Secondary  | ", html_data.second);
+  }
+}
+
+
+
