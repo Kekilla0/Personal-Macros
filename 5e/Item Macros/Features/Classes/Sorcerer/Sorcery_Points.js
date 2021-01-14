433d24751cc07ba3b1fa3bd473fc20f8952b838a
Kekilla0 <64717385+Kekilla0@users.noreply.github.com>
2021-01-13 23:03:39 -0500

    Update Sorcery_Points.js

--- 5e/Item Macros/Features/Classes/Sorcerer/Sorcery_Points.js
+++ 5e/Item Macros/Features/Classes/Sorcerer/Sorcery_Points.js
@@ -10,7 +10,7 @@
 let log = (...args) => console.log("Sorcery Point Macro | ", ...args);
 let wait = async (ms) => new Promise((resolve)=> setTimeout(resolve, ms));
 let macroActor = character !== null ? character : token.actor;
-let data = { items : [], spellSlots : [], sorcPoints : {}, spells : [], cost : [2,3,5,6,7]};
+let data = { items : [], spellSlots : [], sorcPoints : {}, cost : [2,3,5,6,7], spellDiff : false, pointDiff : false};
 let display_message = true;
 let dialog_id = randomID();
 
@@ -21,8 +21,6 @@ let dialog_id = randomID();
 
   updateDialog();
 })();
-
-  //add more information
 
   //execute choice
 
@@ -53,25 +51,20 @@ async function dataCheck()
     else data.items.push({_id : item.id, name});
   });
 
-  Object.entries(macroActor.data.data.spells).forEach(([key, {value, max}])=>{
-    if(key !== `pact` && max !== 0) data.spellSlots.push({ slot : parseInt(key.charAt(5)), value, max });
-    if(key === `pact` && max !== 0) data.spellSlots.push({ slot : `pact`, value, max });
+  Object.entries(macroActor.data.data.spells).filter(([key, value])=> key !== `spell0`).forEach(([key, {value, max}])=>{
+		data.spellSlots.push({ slot : key === `pact` ? key : parseInt(key.charAt(5)), value, max });
   });
 
-  if(data.items.find(i=> i.name === `Sorcerer`)._id === null) return error(`${macroActor.name} does not have a level in Sorcerer`);
-  if(data.items.find(i=> i.name === `Font of Magic`)._id === null) return error(`${macroActor.name} does not have the Font of Magic item/feature.`);
+  if(!checkAction(`Sorcerer`)) return error(`${macroActor.name} does not have a level in Sorcerer`);
+  if(!checkAction(`Font of Magic`)) return error(`${macroActor.name} does not have the Font of Magic item/feature.`);
 
   await fixFont();
-
-  // add checkSlotsAvailable, checkSlotsMissing here --- maybe?
-
-  // populate a new variable in data --- one for spells
 
   async function fixFont()
   {
     let font = macroActor.items.getName("Font of Magic");
     let { levels } = macroActor.items.getName("Sorcerer").data.data;
-    let { uses} = font.data.data;
+    let { uses } = font.data.data;
 
     if(uses.max !== levels)
     {
@@ -80,7 +73,9 @@ async function dataCheck()
       uses.value = levels - difference;
       await font.update({ "data.uses" : uses });
     }
-    data.sorcPoints = uses;
+		data.sorcPoints = uses.value !== null ? uses : {...uses, value : 0 };
+		
+		log(data.sorcPoints);
   }
 }
 function checkSlotsAvailable()
@@ -103,13 +98,11 @@ async function updateDialog()
   interval_time = 0.2;
 
   content = getContent();
-  buttons = { Ok : { label : `Ok`, callback : (html) => { executeHTML(html); }}, Cancel : { label : `Cancel` } };
-
+	buttons = { Cancel : { label : `Cancel` }, Ok : { label : `Ok`, callback : (html) => { executeHTML(html); }}};
   dialog = new Dialog({ title : `Sorcerer Point Macro`, content, buttons, close : ()=> { clearInterval(interval); }}).render(true);
 
   interval = setInterval(()=>{
-    let {action} = getHTML();
-
+		let {action} = getHTML();
     if(last_action !== action)
     {
       last_action = action;
@@ -126,20 +119,90 @@ async function updateDialog()
         <h3 style="flex : 1; justify-content:flex-end">${macroActor.data.name} </h3>
       </div>
       <div style="display:flex; flex-direction:row; justify-content:space-between; align-items:center; flex-grow:1;">
-        <h3 style="flex : 3; justify-content:flex-start;">Spell Slots : ${data.spellSlots.map(ss=> ss.value).join(`/`)}</h3>
+        <h3 style="flex : 3; justify-content:flex-start;">Spell Slots : ${data.spellSlots.filter(ss=> ss.value > 0).map(ss=> ss.value).join(`/`)}</h3>
         <h3 style="flex : 2; justify-content:flex-end;">Sorcery Points : ${data.sorcPoints.value}/${data.sorcPoints.max}</h3>
       </div>
       ${getActions()}
       ${getSecondaryActions()}
-    </div>`;
+		</div>`;
+		
+		function getActions()
+		{
+			let { action, second } = getHTML();
+	
+			return `
+				<div style="display:flex; flex-direction:row; justify-content:space-between; align-items:center; flex-grow:1;">
+					<h3 style="flex : 1; justify-content:flex-start;"><label>Select Action : </label></h3>
+					<h3 style="flex : 1; justify-content:flex-end;">
+						<select id="${dialog_id}actions">
+							${(data.sorcPoints.value !== data.sorcPoints.max && checkSlotsAvailable()) ? `<option ${action === `sp` ? `selected` : ``} value="sp">Convert Spell Slots</option>` : ``}
+							${(data.sorcPoints.value > 2 && checkSlotsMissing()) ? `<option ${action === `ss` ? `selected` : ``} value="ss">Create Spell Slots</option>` : ``}
+							${(data.sorcPoints.value > 0 && checkAction(`Metamagic: Careful Spell`)) ? `<option ${action === `careful` ? `selected` : ``} value="careful">Careful Spell</option>` : ``}
+							${(data.sorcPoints.value > 0 && checkAction(`Metamagic: Distant Spell`)) ? `<option ${action === `distant` ? `selected` : ``} value="distant">Distant Spell</option>` : ``}
+							${(data.sorcPoints.value > 0 && checkAction(`Metamagic: Empowered Spell`)) ? `<option ${action === `empowered` ? `selected` : ``} value="empowered">Empowered Spell</option>` : ``}
+							${(data.sorcPoints.value > 0 && checkAction(`Metamagic: Extended Spell`)) ? `<option ${action === `extended` ? `selected` : ``} value="extended">Extended Spell</option>` : ``}
+							${(data.sorcPoints.value > 2 && checkAction(`Metamagic: Heightened Spell`)) ? `<option ${action === `heightened` ? `selected` : ``} value="heightened">Heightened Spell</option>`: ``}
+							${(data.sorcPoints.value > 1 && checkAction(`Metamagic: Quickened Spell`)) ? `<option ${action === `quickened` ? `selected` : ``} value="quickened">Quickened Spell</option>` : ``}
+							${(data.sorcPoints.value > 1 && checkAction(`Metamagic: Subtle Spell`)) ? `<option ${action === `subtle` ? `selected` : ``} value="subtle">Subtle Spell</option>` : ``}
+							${(data.sorcPoints.value > 0 && checkAction(`Metamagic: Twinned Spell`)) ? `<option ${action === `twinned` ? `selected` : ``} value="twinned">Twinned Spell</option>` : ``}
+						</select>
+					</h3>
+				</div>
+				`;
+		}
+		function getSecondaryActions()
+		{
+			let { action, second } = getHTML(), label = ``, options = ``;
+	
+			switch(action)
+			{
+				case `sp` :
+				case `twinned` :
+					label = `Pick Spell Slot : `;
+					options = data.spellSlots
+						.filter(({value})=> value!==0)
+						.map(({slot})=>`<option ${second === slot.toString() ? `selected` : ``} value="${slot}">Level ${slot}</option>`).join(``);
+	
+					if(action === `twinned`)
+						options = `<option ${second === "0" ? `selected` : ``} value="0">Cantrip</option>${options}`;
+					break;
+				case `ss` :
+					label = `Pick Spell Slot : `;
+					options = data.spellSlots
+						.filter(({slot, value, max})=> slot < 6 && data.cost[slot-1] <= data.sorcPoints.value && value < max)
+						.map(({slot})=> `<option ${second === slot.toString() ? `selected` : ``} value="${slot}">Level ${slot}</option>`).join(``);
+					break;
+				case `careful` :
+				case `distant` : 
+				case `empowered` : 
+				case `extended` :
+				case `heightened` :
+				case `quickened` :
+				case `subtle` :
+					label = options = ``;
+					break;
+				default : 
+					label = options = `Error`;
+					break;
+			}
+	
+			return label === `` ? `` : `
+				<div style="display:flex; flex-direction:row; justify-content:space-between; align-items:center; flex-grow:1;">
+					<h3 style="flex : 1; justify-content:flex-start;"><label>${label}</label></h3>
+					<h3 style="flex : 1; justify-content:flex-end;">
+						<select id="${dialog_id}secondary">
+							${options}
+						</select>
+					</h3>
+				</div>
+				`;
+		}
   }
-
   function update()
   {
     dialog.data.content = getContent();
     dialog.render(true);
   }
-  
   function getHTML()
   {
     let action = $.find(`#${dialog_id}actions`)[0]?.value;
@@ -147,97 +210,97 @@ async function updateDialog()
 
     return { action, second };
   }
-
-  function getActions()
-  {
-    let { action, second } = getHTML();
-
-    return `
-      <div style="display:flex; flex-direction:row; justify-content:space-between; align-items:center; flex-grow:1;">
-        <h3 style="flex : 1; justify-content:flex-start;"><label>Select Action : </label></h3>
-        <h3 style="flex : 1; justify-content:flex-end;">
-          <select id="${dialog_id}actions">
-            ${(data.sorcPoints.value !== data.sorcPoints.max && checkSlotsAvailable()) ? `<option ${action === `sp` ? `selected` : ``} value="sp">Spell Slot => Sorcery Point</option>` : ``}
-            ${(data.sorcPoints.value > 2 && checkSlotsMissing()) ? `<option ${action === `ss` ? `selected` : ``} value="ss">Sorcery Point => Spell Slot</option>` : ``}
-            ${(data.sorcPoints.value > 0 && checkAction(`Metamagic: Careful Spell`)) ? `<option ${action === `cr` ? `selected` : ``} value="cr">Careful Spell</option>` : ``}
-            ${(data.sorcPoints.value > 0 && checkAction(`Metamagic: Distant Spell`)) ? `<option ${action === `ds` ? `selected` : ``} value="ds">Distant Spell</option>` : ``}
-            ${(data.sorcPoints.value > 0 && checkAction(`Metamagic: Empowered Spell`)) ? `<option ${action === `em` ? `selected` : ``} value="em">Empowered Spell</option>` : ``}
-            ${(data.sorcPoints.value > 0 && checkAction(`Metamagic: Extended Spell`)) ? `<option ${action === `ex` ? `selected` : ``} value="ex">Extended Spell</option>` : ``}
-            ${(data.sorcPoints.value > 3 && checkAction(`Metamagic: Heightened Spell`)) ? `<option ${action === `hi` ? `selected` : ``} value="hi">Heightened Spell</option>`: ``}
-            ${(data.sorcPoints.value > 1 && checkAction(`Metamagic: Quickened Spell`)) ? `<option ${action === `qu` ? `selected` : ``} value="qu">Quickened Spell</option>` : ``}
-            ${(data.sorcPoints.value > 1 && checkAction(`Metamagic: Subtle Spell`)) ? `<option ${action === `sb` ? `selected` : ``} value="sb">Subtle Spell</option>` : ``}
-            ${(data.sorcPoints.value > 0 && checkAction(`Metamagic: Twinned Spell`)) ? `<option ${action === `tw` ? `selected` : ``} value="tw">Twinned Spell</option>` : ``}
-          </select>
-        </h3>
-      </div>
-      `;
-  }
-  function getSecondaryActions()
-  {
-    let { action, second } = getHTML(), label = ``, options = ``;
-
-    // build secondary list for dialog
-    switch(action)
-    {
-      case `sp` :
-      case `tw` :
-        label = `Pick Spell Slot : `;
-        options = data.spellSlots
-          .filter(({value})=> value!==0)
-          .map(({slot})=>`<option ${second === slot.toString() ? `selected` : ``} value="${slot}">Level ${slot}</option>`).join(``);
-
-        if(action === `tw`)
-          options = `<option ${second === "0" ? `selected` : ``} value="0">Cantrip</option>${options}`;
-        break;
-      case `ss` :
-        label = `Pick Spell Slot : `;
-        options = data.spellSlots
-          .filter(({slot, value, max})=> slot < 6 && data.cost[slot-1] <= data.sorcPoints.value && value < max)
-          .map(({slot})=> `<option ${second === slot.toString() ? `selected` : ``} value="${slot}">Level ${slot}</option>`).join(``);
-        break;
-      case `cr` :
-      case `ds` : 
-      case `em` : 
-      case `ex` :
-      case `hi` :
-      case `qu` :
-      case `sb` :
-        label = options = ``;
-        break;
-      default : 
-        label = options = `Error`;
-        break;
-    }
-
-    return label === `` ? `` : `
-      <div style="display:flex; flex-direction:row; justify-content:space-between; align-items:center; flex-grow:1;">
-        <h3 style="flex : 1; justify-content:flex-start;"><label>${label}</label></h3>
-        <h3 style="flex : 1; justify-content:flex-end;">
-          <select id="${dialog_id}secondary">
-            ${options}
-          </select>
-        </h3>
-      </div>
-      `;
-  }
-  function checkAction(name)
-  {
-    return data.items.find(i=>i.name === name)._id !== null;
-  }
   async function executeHTML(html)
   {
     let html_data = {
       action : html.find(`#${dialog_id}actions`)[0]?.value,
       second : html.find(`#${dialog_id}secondary`)[0]?.value
-    };
-
-
-    //execute and update based on what the user chose
+		};
+		let level = 0;
 
     log("Action     | ", html_data.action);
-    log("Secondary  | ", html_data.second);
-  }
-}
+		log("Secondary  | ", html_data.second);
 
+		switch(html_data.action)
+		{
+			case `sp` :
+				level = parseInt(html_data.second);	
+				editSpellSlots(level, -1);
+				editSorceryPoints(level);
+				break;
+			case `twinned` :
+			case `ss` :
+				level = parseInt(html_data.second);
+				if(html_data.action === `ss`)
+					editSpellSlots(level,+1);
+				editSorceryPoints((data.cost[level]-1)*-1);
+				break;
+			case `careful` :
+			case `distant` : 
+			case `empowered` : 
+			case `extended` :
+				editSorceryPoints(-1);
+				break;
+			case `quickened` :
+			case `subtle` :
+				editSorceryPoints(-2);
+				break;
+			case `heightened` :
+				editSorceryPoints(-3);
+				break;
+			default : 
+				break;
+		}
+
+		await updateActor();
+		await executeItem(html_data.action);
+	}
+}
+function checkAction(name)
+{
+	return data.items.find(i=>i.name === name)._id !== null;
+}
+function editSorceryPoints(num)
+{
+	data.sorcPoints.value = Math.clamped(data.sorcPoints.value + num, 0, data.sorcPoints.max);
+	data.pointDiff = true;
+}
+function editSpellSlots(level, num)
+{
+	let obj = {value, max, slot} = data.spellSlots.find(d=> d.slot === level);
+	let index = data.spellSlots.indexOf(obj);
+	data.spellSlots[index].value = Math.clamped(value + num, 0, max);
+	data.spellSlots[index].diff = true;
+	data.spellDiff = true;
+}
+async function executeItem(item_code)
+{
+	if(!display_message || !item_code  || item_code === `ss` || item_code === `sp` ) return;
+	let item_name = data.items.find(i=>i.name.toLowerCase().includes(item_code))?.name;	
+
+	if(!item_name) return error(`Failed to find ${item_code}`);
+
+	await macroActor.items.getName(item_name)?.roll();
+}
+async function updateActor()
+{
+	if(data.pointDiff)
+	{
+		let uses = duplicate(data.sorcPoints);
+		await macroActor.items.getName("Font of Magic").update({ "data.uses" : uses });
+	}
+
+	if(data.spellDiff)
+	{
+		let spells = {};
+		data.spellSlots.filter(d=> d.diff).forEach(d=> {
+			let key = d.slot === `pact` ? `pact` : `spell${d.slot}`;
+			spells[key] = { value : d.value };
+		});
+
+		if(spells === {}) return;
+		await macroActor.update({"data.spells" : spells});
+	}
+}
 
 
