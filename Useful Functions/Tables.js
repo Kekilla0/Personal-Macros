6df1f3213f1ea696c5c12f8219ef8b9ede2c42a6
Kekilla0 <64717385+Kekilla0@users.noreply.github.com>
2021-02-13 19:53:18 -0500

    Update Tables.js

--- Useful Functions/Tables.js
+++ Useful Functions/Tables.js
@@ -5,3 +5,42 @@ function getTableText({name  = ``} = {})
 {
   return game.tables.getName(name).roll().results[0].text;
 }
+
+/*
+  Wandering Monster Table : creates a table that has all of the NPC entities inside it.
+*/
+function wanderingMonster()
+{
+  const info = {
+    tableName : `Wandering Monsters`,
+    tableData : {
+      range : -1, type : 1, weight : 1
+    }
+  };
+  let actors = game.actors.filter(a=>a.data.type !== `character`);
+  let data = createData(actors);
+  RollTable.create(data);
+
+  function createData(arr = [])
+  {
+    return {
+      name : info.tableName,
+      formula : `d${arr.length}`,
+      replacement : true,
+      displayRoll : true,
+      results : arr.map(content=>{
+        info.tableData.range++;
+        return {
+          collection : content.entity,
+          drawn : false,
+          img : content.img,
+          range : [info.tableData.range, info.tableData.range],
+          resultId : content.id,
+          text : content.name,
+          type : info.tableData.type,
+          weight : info.tableData.weight
+        };
+      })
+    };
+  }
+}
