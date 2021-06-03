ee6094a4aa28b776264df7d5df6d6f9de9469c66
Kekilla0 <64717385+Kekilla0@users.noreply.github.com>
2021-06-02 21:32:50 -0400

    Update ActiveEffects.js

--- Useful Functions/ActiveEffects.js
+++ Useful Functions/ActiveEffects.js
@@ -3,17 +3,17 @@ async function removeEffect({ actor, effectLabel }){
   let effect = actor.effects.find(e => e.data.label === effectLabel);
   if(!effect) return;
 
-  await actor.deleteEmbeddedEntity("ActiveEffect", [effect.id]);
+  await actor.deleteEmbeddedDocuments("ActiveEffect", [effect.id]);
 }
 
 async function addEffect({ actor, effectData }){
   if(!actor || !effectData) return;
-  await actor.createEmbeddedEntity("ActiveEffect", effectData );
+  await actor.createEmbeddedDocuments("ActiveEffect", [effectData]);
 } 
 
 async function toggleEffect({ actor, effectLabel }){
   if(!actor || !effectLabel) return;
   let effect = actor.effects.find(e => e.data.label === effectLabel);
   if(!effect) return;
-  return await effect.update({ disabled : !effect.data.disabled})
+  return await effect.update({ disabled : !effect.data.disabled});
 }
