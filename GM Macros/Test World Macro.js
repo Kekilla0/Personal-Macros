6626d5596d67c2748b7ea0b17678111e2de0d85c
Matthew Monahan <64717385+Kekilla0@users.noreply.github.com>
2021-12-04 19:45:51 -0500

    Update Test World Macro.js

--- GM Macros/Test World Macro.js
+++ GM Macros/Test World Macro.js
@@ -1,12 +1,19 @@
-//create test scene
-await Scene.create({ name : "Test Scene" });
+//create user
+const user = await User.create({ name : "Test User" });
 
+//create test scene
+const scene = await Scene.create({ name : "Test Scene" });
 //create Actors
-const actors = Object.keys(CONFIG.Actor.typeLabels);
-for(let actor of actors)
-    await Actor.create({ name : `Test ${actor}`, type : actor });
+const actors = [];
+for(let actor of Object.keys(CONFIG.Actor.typeLabels))
+    actors.push(await Actor.create({ name : `Test ${actor}`, type : actor }));
 
 //create Items
-const items = Object.keys(CONFIG.Item.typeLabels);
-for(let item of items)
-    await Item.create({ name : `Test ${item}`, type : item });
+const items = [];
+for(let item of Object.keys(CONFIG.Item.typeLabels))
+    items.push(await Item.create({ name : `Test ${item}`, type : item }));
+
+//update entities
+await user.update({ character : actors[0].id });
+await actors[0].update({ "permission.default" : 3 });
+await actors[0].createEmbeddedDocuments("Item", items.map(item => item.data), {});
