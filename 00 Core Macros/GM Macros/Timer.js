092e5c4a5bf9cc6e531a221f99ce139d6e4f639c
Matthew Monahan <64717385+Kekilla0@users.noreply.github.com>
2022-01-17 22:25:03 -0500

    Update Timer.js

--- 00 Core Macros/GM Macros/Timer.js
+++ 00 Core Macros/GM Macros/Timer.js
@@ -1,22 +1,26 @@
 let seconds_selected = 0;
 let seconds_left = 0;
+let seconds_start = 0;
 
 let times = {
   buttons : [
-    ["1 Min", ()=> { seconds_selected = seconds_left = 60; update_dialog();}],
-    ["5 Min", ()=> { seconds_selected = seconds_left = 300; update_dialog();}],
-    ["10 Min", ()=> { seconds_selected = seconds_left = 600; update_dialog(); }],
-    ["15 Min", ()=> { seconds_selected = seconds_left = 900; update_dialog(); }],
-    ["20 Min", ()=> { seconds_selected = seconds_left = 1200; update_dialog(); }],
-    ["30 Min", ()=> { seconds_selected = seconds_left = 1800; update_dialog(); }]
+    ["1 Min", ()=> { seconds_selected = seconds_left = 60000; seconds_start = Date.now(); update_dialog();}],
+    ["5 Min", ()=> { seconds_selected = seconds_left = 300000; seconds_start = Date.now(); update_dialog();}],
+    ["10 Min", ()=> { seconds_selected = seconds_left = 600000; seconds_start = Date.now(); update_dialog(); }],
+    ["15 Min", ()=> { seconds_selected = seconds_left = 900000; seconds_start = Date.now(); update_dialog(); }],
+    ["20 Min", ()=> { seconds_selected = seconds_left = 1200000; seconds_start = Date.now(); update_dialog(); }],
+    ["30 Min", ()=> { seconds_selected = seconds_left = 1800000; seconds_start = Date.now(); update_dialog(); }],
+    ["1 Hour", ()=> { seconds_selected = seconds_left = 3600000; seconds_start = Date.now(); update_dialog(); }],
+    ["2 Hour", ()=> { seconds_selected = seconds_left = 7200000; seconds_start = Date.now(); update_dialog(); }],
+    ["3 Hour", ()=> { seconds_selected = seconds_left = 10800000; seconds_start = Date.now(); update_dialog(); }],
+    ["4 Hour", ()=> { seconds_selected = seconds_left = 14400000; seconds_start = Date.now(); update_dialog(); }],
   ],
   content : `<div sytle="width:100%; text-align:center;"><h2> Choose a timer limit : </h2></div>`,
   title : `Timer Selector`
 };
 
-async function button_dialog(data)
-{
-  let value = await new Promise((resolve) => {
+async function buttonDialog(data){
+  return await new Promise(async (resolve) => {
     let buttons = {}, dialog;
 
     data.buttons.forEach(([str, callback])=>{
@@ -26,13 +30,21 @@ async function button_dialog(data)
       }
     });
   
-    dialog = new Dialog({title : data.title , content : data.content, buttons, close : () => resolve(true) }).render(true);
+    dialog = new Dialog({
+      title : data.title , 
+      content : data.content, 
+      buttons, 
+      close : () => resolve(true) 
+    },{
+      width : 225, height : 75 + ( 35 * data.buttons.length)
+    });
+
+    await dialog._render(true);
+    dialog.element.find('.dialog-buttons').css({'flex-direction':'column'});
   });
-  return value;
 }
 
-async function update_dialog()
-{
+async function update_dialog(){
   let interval, content, buttons, dialog, interval_time;
 
   interval_time = 1;
@@ -47,41 +59,40 @@ async function update_dialog()
     update();
   }, interval_time * 1000);
 
-  function getContent()
-  {
-
-    if(seconds_left !== 0 )
-    {
-      let minutes = Math.floor(seconds_left/60);
+  function getContent(){
+    if(seconds_left !== 0 ){
+      seconds_left = Math.floor(seconds_left / 1000);
       let seconds = seconds_left%60;
+      let minutes_left = Math.floor(seconds_left/60);
+      let minutes = minutes_left%60;
+      let hours = Math.floor(minutes_left/60);      
   
+      if (hours < 10) hours = `0${hours}`;
       if (minutes < 10) minutes = `0${minutes}`;
       if (seconds < 10) seconds = `0${seconds}`;
 
-      return `<div sytle="width:100%; text-align:center;"><h1>${minutes}:${seconds}</h1></div>`;
+      return `<div sytle="width:100%; text-align:center;"><h1>${hours}:${minutes}:${seconds}</h1></div>`;
     }else{
-      if(dialog.data.content.includes(`Complete`))
-      {
+      if(dialog.data.content.includes(`Complete`)){
         return ``;
       }else{
         return `<div sytle="width:100%; text-align:center; color:red;"><h1>Complete</h1></div>`;
       }
     }
   }
-  function getButtons()
-  {
+  function getButtons(){
     return {
+      Reset : { label : `Reset`, callback : () => { buttonDialog(times); }},
       Restart : { label : `Restart`, callback : () => { seconds_left = seconds_selected; update_dialog(); }},
       Cancel : { label : `Cancel`, callback : () => { clearInterval(interval); }}
     };
   }
-  function update()
-  {
-    if(seconds_left !== 0) seconds_left--;
+  function update(){
+    if(seconds_left !== 0) seconds_left = seconds_selected - (Date.now() - seconds_start);
 
     dialog.data.content = getContent();
     dialog.render(true);
   }
 }
 
-button_dialog(times);
+buttonDialog(times);
